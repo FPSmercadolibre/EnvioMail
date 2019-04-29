f55e4c39bdbd457a2775dcb73aa5f94dcded638c
christian hernan schettino <cschettino@ml.com>
2019-04-29 14:40:57 -0300

    Initial commit

---
+++ envio_mail.py
@@ -0,0 +1,25 @@
+import smtplib, ssl
+import threading
+
+interval = 5
+
+def send():
+    #threading.Timer(interval, send).start()
+    port = 587  # For starttls
+    smtp_server = "smtp.gmail.com"
+    sender_email = ""#eMail
+    receiver_email = input('Ingrese el mail destinatario:') 
+    password = "" #Pass
+    message = """\
+    Subject: Este mensaje fue enviado por Python."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo()  # Can be omitted
+        server.starttls(context=context)
+        server.ehlo()  # Can be omitted
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+send()
+print('El mensaje fue enviado correctamente!')
